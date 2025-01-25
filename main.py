afecd58410107a6f96e061510e588b3351ca6017
tata0409 <tatachechyna@gmail.com>
2025-01-24 23:25:31 -0500

    KMeans with 2 centroids

--- main.py
+++ main.py
@@ -6,7 +6,21 @@ np.random.seed(42)
 X = np.vstack((np.random.randn(50, 2) + [2, 2], np.random.randn(50, 2) + [7, 7]))
 
 plt.scatter(X[:, 0], X[:, 1], s=50, color='gray')
-plt.title("Вихідні дані")
+plt.title("Output")
 plt.xlabel("X1")
 plt.ylabel("X2")
+plt.show()
+
+kmeans = KMeans(n_clusters=2, random_state=42)
+kmeans.fit(X)
+
+labels = kmeans.labels_
+centroids = kmeans.cluster_centers_
+
+plt.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis', s=50)
+plt.scatter(centroids[:,0], centroids[:, 1], c='red', marker='x', s=200, label='Centroid')
+plt.title("Results of clusterization")
+plt.xlabel("X1")
+plt.ylabel("X2")
+plt.legend()
 plt.show()
